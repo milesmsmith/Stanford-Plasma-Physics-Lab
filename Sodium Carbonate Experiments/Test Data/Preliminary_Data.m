fe685186f6e1186dc540a52a6e5da76d4f396407
milesmsmith <milessmith.engineering@gmail.com>
2022-06-10 09:09:41 -0700

    All the files!

---
+++ Sodium Carbonate Experiments/Test Data/Preliminary_Data.m
@@ -0,0 +1,6 @@
+% Title: Preliminary_Data.m
+% Author: Miles Smith
+% Date: 9 Jun 2022
+
+control_data = readmatrix('20220609_131609_cont.csv');
+experiment_data = readmatrix('20220609_133428_carbonate.csv');
